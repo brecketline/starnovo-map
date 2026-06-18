7870058f278deec3d7f5ec36e38513a4bbb18460
Brenda <brenda.2434702@aluno.faculdadedescomplica.com.br>
2026-06-18 15:07:12 +0200

    phase 2: medieval parchment map style

--- src/components/Map/MapView.jsx
+++ src/components/Map/MapView.jsx
@@ -2,6 +2,39 @@ import { useEffect, useRef } from 'react'
 import maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 
+const medievalStyle = {
+  version: 8,
+  name: 'Starnovo Medieval',
+  sources: {
+    osm: {
+      type: 'raster',
+      tiles: ['https://tile.openstreetmap.org/{z}/{x}/{y}.png'],
+      tileSize: 256,
+      attribution: '© OpenStreetMap contributors'
+    }
+  },
+  layers: [
+    {
+      id: 'background',
+      type: 'background',
+      paint: {
+        'background-color': '#F4E8C1'
+      }
+    },
+    {
+      id: 'osm-tiles',
+      type: 'raster',
+      source: 'osm',
+      paint: {
+        'raster-opacity': 0.3,
+        'raster-saturation': -1,
+        'raster-contrast': 0.2,
+        'raster-brightness-min': 0.8
+      }
+    }
+  ]
+}
+
 function MapView() {
   const mapContainer = useRef(null)
   const map = useRef(null)
@@ -11,7 +44,7 @@ function MapView() {
 
     map.current = new maplibregl.Map({
       container: mapContainer.current,
-      style: 'https://tiles.openfreemap.org/styles/liberty',
+      style: medievalStyle,
       center: [25.6525, 43.0818],
       zoom: 15
     })
